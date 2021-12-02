533c1fc91cf1e65c3ba9473711d95d1dba2435ae
raheela2000 <72557352+raheela2000@users.noreply.github.com>
2021-12-02 16:25:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+
+provider "aws" {
+  access_key = "AKIARWIQKKJQX7IAJEE5"
+  secret_key = "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"
+  region = var.region
+ # shared_credentials_file = var.creds
+  profile = "default"
+}
+
+# Create a VPC
+resource "aws_vpc" "app_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "app-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  tags = {
+    Name = "vpc_igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.app_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-03d5c68bab01f3496"
+  instance_type = var.instance_type
+  key_name = "terraform"
+  subnet_id              = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo yum update -y
+  sudo yum install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
+
+  tags = {
+    Name = "web_instance"
+  }
+
+  volume_tags = {
+    Name = "web_instance"
+  }
+}
